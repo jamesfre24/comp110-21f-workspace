59e183fceb135b066f6643a91faf14a8fc4c08c9
James Freeland <xan.freeland@gmail.com>
2021-11-04 19:22:48 -0400

    Progress on Exercise 6

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,41 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730699395"
 
-# Define your functions below
+
+def invert(Input_string: dict[str, str]) -> dict[str, str]:
+    """Inverts the keys and values of a given dictionary"""
+    Inversion: dict[str, str] = {}
+    for key in Input_string:
+        if Input_string[key] in Inversion:
+            raise KeyError("You cannot have multiple of the same key")
+        Inversion[Input_string[key]] = key
+    return Inversion
+
+
+def favorite_color(color: dict[str, str]) -> str:
+    """Returns a string of the color that appears most frequently"""
+    most_frequent: str = ""
+    i: int = 0
+    dictionary: dict[str, int] = {}
+    for name in color:
+        dictionary[color[name]] = 1
+    for name in color:
+        if color[name] in dictionary:
+            dictionary[color[name]] += 1
+            most_frequent = color[name]
+    for name in dictionary:
+        if dictionary[name] > i:
+            i = dictionary[color[name]]
+            most_frequent = name
+    return most_frequent
+
+
+def count(key_list: list[str]) -> dict[str, int]:
+    """Counts the frequencies of keys in a list"""
+    dictionary: dict[str, int] = {}
+    for input_value in key_list:
+        dictionary[input_value] = 0
+    for input_value in dictionary:
+        dictionary[input_value] += 1
+    return dictionary
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,58 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730699395"
+
+
+def test_invert_edge() -> None:
+    """One key and value edge test of the dictionary invert function"""
+    input: dict[str, str] = {'a': 'b'}
+    assert invert(input) == {'b': 'a'}
+
+
+def test_invert_use_1() -> None:
+    """Use test for invert with multiple keys and values"""
+    input: dict[str, str] = {'a': 'b', 'c': 'd', 'e': 'f'}
+    assert invert(input) == {'b': 'a', 'd': 'c', 'f': 'a'}
+
+
+def test_invert_use_2() -> None:
+    input: dict[str, str] = {'g': 'h', 'i': 'j', 'k': 'l'}
+    assert invert(input) == {'h': 'g', 'j': 'i', 'l': 'k'}
+
+
+def test_favorite_color_edge() -> None:
+    """Edge test for favorite color with one name and color"""
+    color: dict[str, str] = {"James": "Green"}
+    assert favorite_color(color) == "Green"
+
+
+def test_favorite_color_use_1() -> None:
+    """Use test 1 for favorite color with multiple names and colors"""
+    color: dict[str, str] = {"James": "Green", "Matthew": "Red"}
+    assert favorite_color(color) == "Green"
+
+
+def test_favorite_color_use_2() -> None:
+    """Use test 2 for favorite color with multiple names and colors"""
+    color: dict[str, str] = {"James": "Green", "Matthew": "Red", "Marsh": "Blue"}
+    assert favorite_color(color) == "Green"
+
+
+def test_count_edge() -> None:
+    """Edge test for count with one value"""
+    countx: list[str] = ['a']
+    assert count(countx) == {'a': 1}
+
+
+def test_count_use_1() -> None:
+    """Use test 1 for count with multiple values"""
+    countx: list[str] = ['a', 'b']
+    assert count(countx) == {'a': 1, 'b': 1}
+
+
+def test_count_use_2() -> None:
+    """Use test 2 for count with multiple values"""
+    countx: list[str] = ['a', 'a', 'b', 'b', 'b', 'c']
+    assert count(countx) == {'a': 2, 'b': 3, 'c': 1}
